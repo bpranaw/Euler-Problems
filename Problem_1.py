001d4041072ea5a2302c5d50b80fd2c21b5ab958
Pranaw Bajracharya <bajrachp@oregonstate.edu>
2019-12-05 18:49:49 -0800

    Added [COMPLETE] Problem 1 program
    The program counts all the numbers from 0-1000 that are divisible by either 3 or 5 and finds their sum.

---
+++ Problem_1.py
@@ -0,0 +1,13 @@
+#Variables(x is the number that are tested for divisibility by 3 or 5, and y is the sum of all those numbers within the range)
+y = 0 
+x = 0
+    
+#Loop that cycles from x=0 to x=1001 (meaning it goes from 0 to 1000)
+for x in range(0,1001):
+    #Checks to see if number is divisible by 3 or 5 evenly
+    if x % 5 == 0 or x % 3 == 0:
+        #If so, it adds current number to the sum
+        y = y + x
+            
+#Prints total sum
+print(y)
